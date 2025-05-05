b7dbbf2ae420b410cab416e48082c33274441f86
Fredrickadler <13fredrick2001@gmail.com>
2025-05-05 17:31:57 +0330

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-900': '#111827',
+        'gray-800': '#1F2937',
+        'blue-100': '#DBEAFE',
+      },
+    },
+  },
+  plugins: [],
+};
